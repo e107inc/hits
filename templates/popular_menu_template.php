1e47c9a46200bf552871e1165e2d73a56a1b70e7
Cameron <e107inc@gmail.com>
2017-03-05 07:45:37 -0800

    Template update

--- templates/popular_menu_template.php
+++ templates/popular_menu_template.php
@@ -13,12 +13,12 @@
 $POPULAR_MENU_TEMPLATE['default']['start'] 	= "<ul class='media-list unstyled list-unstyled popular-menu'>{SETIMAGE: w=100&h=100&crop=1}"; // set the {NEWSIMAGE} dimensions.
 $POPULAR_MENU_TEMPLATE['default']['item'] 	= '<li class="media">
 													  <div class="media-left media-top">
-													    <a href="{NEWS_URL}">
-													      {NEWS_IMAGE: class=media-object img-rounded&placeholder=1}
-													    </a>
+															<a href="{NEWS_URL}">
+													      {NEWS_IMAGE: type=tag&class=media-object img-rounded&placeholder=1}
+															</a>
 													  </div>
 													  <div class="media-body">
-													    <h4 class="media-heading"> <a href="{NEWS_URL}">{NEWS_TITLE}</a></h4>
+													    <h4 class="media-heading">{NEWS_TITLE}</a></h4>
 													    <p>{NEWS_SUMMARY: limit=60}</p>
 													    <small>{GLYPH=fa-stats} {HITS_COUNTER}</small>
 													  </div>
